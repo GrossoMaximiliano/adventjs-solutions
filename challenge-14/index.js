9ed1a1ecc08645a90545008d9a79cad7154aeedb
GrossoMaximiliano <87657806+GrossoMaximiliano@users.noreply.github.com>
2021-12-15 16:17:14 -0300

    Update index.js

--- challenge-14/index.js
+++ challenge-14/index.js
@@ -1,5 +1,5 @@
 export default function missingReindeer(ids) {
-  ids = ids.sort( (n1,n2) => n1-n2 );
+  ids.sort( (n1,n2) => n1-n2 );
   for (let i = 0; i < ids.length; i++){
     if ( ids[i] != i ) return i;
   }
